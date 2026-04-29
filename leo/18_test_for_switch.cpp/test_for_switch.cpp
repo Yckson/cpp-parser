546b2f0da93a5649d29ab9d474017aa32ef6128e
leocaricchio <leocaricchio@academico.ufs.br>
2026-04-28 23:03:14 -0300

    Add files via upload

--- leo/18_test_for_switch.cpp/test_for_switch.cpp
+++ leo/18_test_for_switch.cpp/test_for_switch.cpp
@@ -1,1 +1,18 @@
+int f(int x) {
+    for (int i = 0; i < x; i++) {
+        if (i % 2 == 0) continue;
 
+        try {
+            if (i == 5) throw i;
+        } catch (...) {
+            break;
+        }
+    }
+
+    switch(x) {
+        case 0:
+            return 0;
+        default:
+            return x > 0 ? x : -x;
+    }
+}

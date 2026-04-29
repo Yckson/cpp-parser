62de26366e405a3221ad15a0f5f124d3c75a0b6f
leocaricchio <leocaricchio@academico.ufs.br>
2026-04-28 23:17:07 -0300

    Add files via upload

--- leo/32_try_catch.cpp/try_catch.cpp
+++ leo/32_try_catch.cpp/try_catch.cpp
@@ -1,1 +1,11 @@
-
+int f(int x) {
+    try {
+        if (x == 0) throw 1;
+        if (x < 0) throw 2;
+        return x;
+    } catch (int e) {
+        return -e;
+    } catch (...) {
+        return 0;
+    }
+}

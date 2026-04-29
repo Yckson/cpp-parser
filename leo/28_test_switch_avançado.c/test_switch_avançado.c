203b8dc3b4f6290a79b6a4489fbc8daca7ddf8cf
leocaricchio <leocaricchio@academico.ufs.br>
2026-04-28 23:13:56 -0300

    Add files via upload

--- leo/28_test_switch_avançado.c/test_switch_avançado.c
+++ leo/28_test_switch_avançado.c/test_switch_avançado.c
@@ -1,1 +1,13 @@
-
+int f(int x) {
+    switch(x) {
+        case 1:
+        case 2:
+            x += 1;
+        case 3:
+            x += 2;
+            break;
+        default:
+            x = 0;
+    }
+    return x;
+}

a73ef010364b74e1184ba9b2588b31ef955bb1b4
leocaricchio <leocaricchio@academico.ufs.br>
2026-04-28 23:14:58 -0300

    Add files via upload

--- leo/29_test_switch_case_vazio.c/test_switch_case_vazio.c
+++ leo/29_test_switch_case_vazio.c/test_switch_case_vazio.c
@@ -1,1 +1,13 @@
-
+int f(int x) {
+    switch(x) {
+        case 1:
+        case 2:
+            x++;
+            break;
+        case 3:
+            x += 2;
+        default:
+            x = 0;
+    }
+    return x;
+}
